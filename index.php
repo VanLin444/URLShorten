42c70e9d7d6ee043270ecd23f47d60e517a64d24
Ivan <onel1n.van@yandex.ru>
2024-10-20 23:31:26 +0300

    init

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang = "ru">
+<head>
+    <title>URLShorten</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class = "container text-center">
+        <div class = "row" style = "margin-top: 20%;">
+            <h1>
+                URL SHORTEN
+            </h1>
+        </div>
+        <div class = "row">
+            <form action = "">
+                <div class="input-group mb-3">
+                    <input type="text" name = "url_shorten" class="form-control" placeholder="Вставьте ссылку"  aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Сократить</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+</body>
+</html>
