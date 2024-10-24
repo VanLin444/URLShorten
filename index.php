4c53d9b3920f8c28f9ace3b5a201922ed8971685
Ivan <onel1n.van@yandex.ru>
2024-10-24 23:22:56 +0300

    token_gen

--- index.php
+++ index.php
@@ -7,16 +7,24 @@
 <head>
     <title>URLShorten</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <style>
+        a{
+            text-decoration: none;
+            color: black;
+        }
+    </style>
 </head>
 <body>
     <div class = "container text-center">
         <div class = "row" style = "margin-top: 20%;">
-            <h1>
-                URL SHORTEN
-            </h1>
+            <a href="http://localhost/URLShorten/">
+                <h1>
+                    URL SHORTEN
+                </h1>
+            </a>
         </div>
         <div class = "row">
-            <form action = "" method="POST">
+            <form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
                 <div class="input-group mb-3">
                     <input type="text" name = "url_shorten" class="form-control" placeholder="Вставьте ссылку"  aria-describedby="button-addon2">
                     <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Сократить</button>
--- requests.php
+++ requests.php
@@ -1,16 +1,50 @@
 <?php
     include 'connection.php';
 
-    $request = $_POST['url_shorten'];
-    echo $request;
-    if (!empty($request)){
-        $check = mysqli_query($conn, "SELECT * FROM `links` WHERE link='".$request."'");
-        print_r($check);
+    $request = trim($_POST['url_shorten']);
+    $request = mysqli_real_escape_string($conn, $request);
 
-        if (!mysqli_num_rows($check)){
-            echo "Good";
-        } else {
-            echo "Bad";
+    if(!empty($request)){
+
+        $search_bool = false;
+        $token = '';
+
+        while (!$search_bool){
+            $token = token_gen();
+            $sel = mysqli_query($conn, "SELECT * FROM `links` WHERE `token` ='".$token."'");
+            
+            if(!mysqli_num_rows($sel)){
+                $search_bool = true;
+            }
+    
+        }
+
+        
+        if($search_bool){
+            $ins = mysqli_query($conn, "INSERT INTO `links` (`link`,`token`) VALUES ('".$request."', '".token_gen()."')");
+    
+            if ($ins){
+                echo "Добавлено";
+            } else {
+                echo "Ошибка";
+            }
         }
     }
+
+    
+
+    function token_gen($min = 4, $max = 10 ){
+        $chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789';
+        $new_chars = str_split($chars);
+
+        $token = '';
+        $rand_end = mt_rand($min,$max);
+
+        for($i = 0; $i < $rand_end; $i++){
+            $token .= $new_chars[mt_rand(0, sizeof($new_chars)-1)];
+        }
+
+        return $token;
+    }
+
 ?>
