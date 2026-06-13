7d3820e6ea170fab54dfa7632694e6fbf9a2cb6f
VanLin <onel1n.van@yandex.ru>
2026-06-14 01:02:19 +0300

    Revert "refactoring | directories paths changed"
    This reverts commit 6fb047ad0a7f1edb77a7358b433f8a6c6c67b006.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
-<?php require_once __DIR__ . '/../includes/requests.php'?>
+<?php include __DIR__ . '/requests.php' ?>
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
     <title>URLShorten</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
